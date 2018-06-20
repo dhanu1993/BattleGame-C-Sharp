ac7083693ee958d88eff660c4a2c1e8e1a16caef
dhanu1993 <dhanunjaya.panchireddy@gmail.com>
2018-06-20 14:42:06 +0530

    Delete db_create.py

--- project/db_create.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-from _config import DATABASE_PATH
-with sqlite3.connect(DATABASE_PATH) as connection:
-	c=connection.cursor()
-	c.execute("DROP TABLE IF EXISTS tasks")
-	c.execute(""" CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT , name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL, status INTEGER NOT NULL)""")
-	c.execute('INSERT INTO tasks(name, due_date, priority, status)' 'VALUES("Finish this tutorial", "03/25/2015", 10, 1)')
-	c.execute('INSERT INTO tasks(name, due_date, priority, status)' 'VALUES("Finis Real Pyhton Course 2", "03/25/2015", 10, 1)')
